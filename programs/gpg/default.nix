e13e97d6aeadd4bc46623e0d777a16d463a89519
ZerataX <mail@zera.tax>
2021-10-31 00:31:32 +0000

    fix gnupg config

--- programs/gpg/default.nix
+++ programs/gpg/default.nix
@@ -1,18 +1,17 @@
 { config, lib, pkgs, ... }: {
-  home.packages = with pkgs; [
-    pinentry-curses
-  ];
-
   programs.gpg = {
     enable = true;
     settings = {
-      default-key = "44F7B7979D3A27B189E0841E8333735E784DF9D4";
+      default-key = "0x8333735E784DF9D4";
     };
   };
 
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
-    pinentryFlavor = "curses";
+    # pinentryFlavor = "curses";
+    extraConfig = ''
+      allow-loopback-pinentry
+    '';
   };
 }
