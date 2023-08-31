c3dd272ceda4a3fd07b2535560d63c919da7ea3b
ZerataX <mail@zera.tax>
2023-08-31 23:30:06 +0200

    vscode change extensions

--- programs/vscode/default.nix
+++ programs/vscode/default.nix
@@ -4,14 +4,20 @@
     enable = true;
 
     extensions = with pkgs.vscode-extensions; [
+      ### TOOLS
       # eamodio.gitlens
-      bbenoist.Nix
       # ms-vscode.cpptools # https://github.com/NixOS/nixpkgs/issues/35088
       ms-vscode-remote.remote-ssh
-      scala-lang.scala
-      vscodevim.vim
+      pkgs.nur.repos.zeratax.vscode-extensions.github.copilot
+
+      ## LINTER
       pkgs.nur.repos.zeratax.vscode-extensions.b4dm4n.nixpkgs-fmt
-      pkgs.nur.repos.zeratax.vscode-extensions.hookyqr.beautify
+      # pkgs.nur.repos.zeratax.vscode-extensions.hookyqr.beautify
+
+      ### LANGUAGES
+      scala-lang.scala
+      bbenoist.nix
+      vscodevim.vim
       pkgs.nur.repos.zeratax.vscode-extensions.crystal-lang-tools.crystal-lang
     ];
   };
