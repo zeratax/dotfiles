c2505b2bf4b349a0db82b9d859ac5b2ac8a44db4
ZerataX <mail@zera.tax>
2024-09-25 18:43:32 +0200

    new alacritty config layout

--- programs/alacritty/default.nix
+++ programs/alacritty/default.nix
@@ -3,21 +3,23 @@
 in {
   programs.alacritty.enable = true;
   programs.alacritty.settings = {
-    normal = {
-      family = font;
-      style = "Regular";
-    };
-    bold = {
-      family = font;
-      style = "Bold";
-    };
-    italic = {
-      family = font;
-      style = "Oblique";
-    };
-    bold_italic = {
-      family = font;
-      style = "Bold Oblique";
+    font = {
+      normal = {
+        family = font;
+        style = "Regular";
+      };
+      bold = {
+        family = font;
+        style = "Bold";
+      };
+      italic = {
+        family = font;
+        style = "Oblique";
+      };
+      bold_italic = {
+        family = font;
+        style = "Bold Oblique";
+      };
     };
   };
   fonts.fontconfig.enable = true;
