4b7ba0b4d535ffeb680e8e9cb00dae7433e7e1ee
ZerataX <mail@zera.tax>
2026-04-15 23:14:47 +0200

    use vscode profiles.default.extensions

--- programs/vscode/default.nix
+++ programs/vscode/default.nix
@@ -2,7 +2,7 @@
   programs.vscode = {
     enable = true;
 
-    extensions = with pkgs.vscode-extensions; [
+    profiles.default.extensions = with pkgs.vscode-extensions; [
       ### TOOLS
       ms-vscode-remote.remote-ssh
       github.copilot
