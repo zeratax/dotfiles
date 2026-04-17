bb5303aa5e52f40d4d1142d0d9198df1427ce0eb
ZerataX <mail@zera.tax>
2026-04-17 23:29:05 +0200

    ghostty: add quake-style quick terminal keybind

--- programs/ghostty/default.nix
+++ programs/ghostty/default.nix
@@ -4,5 +4,10 @@
     enableBashIntegration = true;
     installVimSyntax = true;
     systemd.enable = true;
+    settings = {
+      keybind = "global:f12=toggle_quick_terminal";
+      quick-terminal-position = "top";
+      quick-terminal-animation-duration = 0;
+    };
   };
 }
