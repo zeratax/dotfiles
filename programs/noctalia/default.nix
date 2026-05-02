0ab58fe848783bca1a4706443cd8dffce57c34fe
ZerataX <mail@zera.tax>
2026-05-02 04:28:06 +0200

    enable idle for noctalia

--- programs/noctalia/default.nix
+++ programs/noctalia/default.nix
@@ -60,6 +60,20 @@ in {
           {id = "niri"; enabled = true;}
         ];
       };
+      idle = {
+        enabled = true;
+        screenOffTimeout = 600;
+        lockTimeout = 660;
+        suspendTimeout = 1800;
+        fadeDuration = 5;
+        screenOffCommand = "";
+        lockCommand = "";
+        suspendCommand = "";
+        resumeScreenOffCommand = "";
+        resumeLockCommand = "";
+        resumeSuspendCommand = "";
+        customCommands = "[]";
+      };
     };
     plugins = {
       sources = [
